1384fbfd3a2912e232a3612256294e1764dfc742
Umme Kawser Sinthia <40598386+sinthy08@users.noreply.github.com>
2021-09-07 14:12:48 +0600

    Revert "added the index and settings files for the view also fetched values from form to the index and added values in the language file."

--- version.php
+++ version.php
@@ -24,6 +24,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2021090200; // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2021090200; // Requires this Moodle version.
+$plugin->version   = 2021080500; // The current plugin version (Date: YYYYMMDDXX).
+$plugin->requires  = 2019111806; // Requires this Moodle version.
 $plugin->component = 'tool_resetcoursecompletion'; // Full name of the plugin (used for diagnostics).
