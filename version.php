ed7b4071ca7d521a641b7911fdb072df7e883378
Umme Kawser Sinthia <40598386+sinthy08@users.noreply.github.com>
2021-09-22 19:52:11 +0600

    Updated the version as per 3.11 version of  moodle

--- version.php
+++ version.php
@@ -24,6 +24,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2021090200; // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2021051703; // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2019111806; // Requires this Moodle version.
 $plugin->component = 'tool_resetcoursecompletion'; // Full name of the plugin (used for diagnostics).
